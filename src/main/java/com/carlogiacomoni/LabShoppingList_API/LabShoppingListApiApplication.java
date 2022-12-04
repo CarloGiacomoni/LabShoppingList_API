d11f95dbadf70e4b221936b376b2a6ea0558c7ed
Carlo Giacomoni <giacomoni.carlo@gmail.com>
2022-12-04 01:37:58 -0300

    Primeiro Commit

---
+++ src/main/java/com/carlogiacomoni/LabShoppingList_API/LabShoppingListApiApplication.java
@@ -0,0 +1,19 @@
+package com.carlogiacomoni.LabShoppingList_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class LabShoppingListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabShoppingListApiApplication.class, args);
+	}
+	@GetMapping("/")
+	public String index(){
+		return "Olá mundo!";
+	}
+}
---
+++ src/test/java/com/carlogiacomoni/LabShoppingList_API/LabShoppingListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlogiacomoni.LabShoppingList_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabShoppingListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
